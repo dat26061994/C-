2f2ceb6ea601767500b5dd998f9029e062848789
dat26061994 <dat26061994@gmail.com>
2017-05-31 11:41:45 -0700

    master

--- FootbalClubManagement/Program.cs
+++ FootbalClubManagement/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tutorial.SqlConn;
+using MySql.Data.MySqlClient;
 
 namespace FootbalClubManagement
 {
